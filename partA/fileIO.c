770f8b0e17c59b2ade076c1f1e867bb7b9b4dcff
Connor <connor.beardsmore@gmail.com>
2016-05-07 11:44:07 +0800

    updated report

--- partA/fileIO.c
+++ partA/fileIO.c
@@ -17,45 +17,45 @@
 
 int readFile( char* filename, int* matrix, int rows, int cols)
 {
-	int nRead;
-	int offset = 0;
-	int status = 0;
+    int nRead;
+    int offset = 0;
+    int status = 0;
 
-	// OPEN FILE AND CONFIRM NO ERRORS OCCURRED
-	FILE* f = fopen( filename, "r" );
-	if ( f == NULL )
-	{
-		perror( "ERROR - opening file!\n" );
-		return -1;
-	}
+    // OPEN FILE AND CONFIRM NO ERRORS OCCURRED
+    FILE* f = fopen( filename, "r" );
+    if ( f == NULL )
+    {
+        perror( "ERROR - opening file!\n" );
+        return -1;
+    }
 
-	// ITERATE TO FILL ALL MATRIX ROWS
-	for ( int ii = 0; ii < rows; ii++ )
-	{
-		// ITERATE TO FILL ALL MATRIX ROWS
-		offset = ii * cols;
-		for ( int jj = 0; jj < cols; jj++ )
-		{
-			nRead = fscanf( f, "%d", ( &matrix[offset + jj] ) );
-			if ( nRead < 0 )
-			{
-				// CHECK THAT ENOUGH VALUES HAVE BEEN READ
-				if ( (offset + jj) < (rows * cols - 1) )
-				{
-					fprintf( stderr, "ERROR - not enough matrix values" );
-					status = -1;
-				}
-				// CHECK THAT NO ERROR FORCED EARLY EXIT
-				else if ( ferror(f) )
-				{
-					perror("ERROR - reading matrix file!\n");
-					status = -1;
-				}
-			}
-		}
-	}
+    // ITERATE TO FILL ALL MATRIX ROWS
+    for ( int ii = 0; ii < rows; ii++ )
+    {
+        // ITERATE TO FILL ALL MATRIX ROWS
+        offset = ii * cols;
+        for ( int jj = 0; jj < cols; jj++ )
+        {
+            nRead = fscanf( f, "%d", ( &matrix[offset + jj] ) );
+            if ( nRead < 0 )
+            {
+                // CHECK THAT ENOUGH VALUES HAVE BEEN READ
+                if ( (offset + jj) < (rows * cols - 1) )
+                {
+                    fprintf( stderr, "ERROR - not enough matrix values" );
+                    status = -1;
+                }
+                // CHECK THAT NO ERROR FORCED EARLY EXIT
+                else if ( ferror(f) )
+                {
+                    perror("ERROR - reading matrix file!\n");
+                    status = -1;
+                }
+            }
+        }
+    }
 
-	fclose(f);
-	return status;
+    fclose(f);
+    return status;
 }
 //--------------------------------------------------------------------------
